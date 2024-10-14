9c0d802faac86ccb6786512a3f060a55a213b313
sedef <128726482+sedx07@users.noreply.github.com>
2024-10-14 12:23:43 +0300

    Update types_sedef_sert.py

--- Week02/types_sedef_sert.py
+++ Week02/types_sedef_sert.py
@@ -2,8 +2,3 @@ my_int = 43
 my_float = 21.05
 my_bool = True
 my_complex = 6 + 4j
-
-print("my_int:", my_int, " & type:", type(my_int))
-print("my_float:", my_float, " & type:", type(my_float))
-print("my_bool:", my_bool, " & type:", type(my_bool))
-print("my_complex:", my_complex, " & type:", type(my_complex))
